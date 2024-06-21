d72155bd3e22b92d497e06270c9771d6f9b61ed9
AhemOne <36661387+AhemOne@users.noreply.github.com>
2024-06-21 17:52:44 +1000

    Create run.js

---
+++ run.js
@@ -0,0 +1,25 @@
+function parse() {
+  const fileref = document.getElementById('file').files[0];
+	const reader = new FileReader();
+	
+	reader.onload = e => {
+		(new MacResourceFork(e.target.result)).then(fork=>{
+			console.log('fork', fork);
+			if ( fork.PICT !== undefined ) {
+				for ( var id in fork.PICT ) {
+					//console.log(">> resource:", id)
+					const picture = new PICT(fork.PICT[id].raw);
+          const img = document.createElement('img');
+          img.src = picture.dataURL;
+          document.body.append(img);
+				}
+			}
+		});
+	}
+		
+	reader.readAsArrayBuffer(fileref);
+}
+
+function setup() {
+  document.getElementById('loadbutton').onclick = parse;
+}
