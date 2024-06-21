448c0a7785e2536efb308ae7b316fabfd8daf57c
AhemOne <36661387+AhemOne@users.noreply.github.com>
2024-06-21 17:59:49 +1000

    Update run.js

--- run.js
+++ run.js
@@ -1,17 +1,17 @@
 function parse() {
-  const fileref = document.getElementById('file').files[0];
+	const fileref = document.getElementById('file').files[0];
 	const reader = new FileReader();
 	
 	reader.onload = e => {
-    (new MacResourceFork(e.target.result)).then(fork=>{
-      console.log('fork', fork);
-      if ( fork.PICT !== undefined ) {
-        for ( var id in fork.PICT ) {
-          console.log('found PICT id', id);
+		(new MacResourceFork(e.target.result)).then(fork=>{
+			console.log('fork', fork);
+			if ( fork.PICT !== undefined ) {
+				for ( var id in fork.PICT ) {
+					console.log('found PICT id', id);
 					const picture = new PICT(fork.PICT[id].raw);
-          const img = document.createElement('img');
-          img.src = picture.dataURL;
-          document.body.append(img);
+					const img = document.createElement('img');
+					img.src = picture.dataURL;
+					document.body.append(img);
 				}
 			}
 		});
@@ -21,6 +21,6 @@ function parse() {
 }
 
 function setup() {
-  document.getElementById('loadbutton').onclick = parse;	
-  console.log('loader setup complete');
+	document.getElementById('loadbutton').onclick = parse;	
+	console.log('loader setup complete');
 }
